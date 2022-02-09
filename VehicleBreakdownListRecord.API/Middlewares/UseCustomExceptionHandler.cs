7888d12b73dff11945f0e672dfad38816286133f
Metehan Zorluoglu <metehanzorluoglu@gmail.com>
2022-02-09 20:38:27 +0300

    Adding Custom Middleware for ExceptionHandler

---
+++ VehicleBreakdownListRecord.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using VehicleBreakdownRecor.Business.Exceptions;
+using VehicleBreakdownRecord.Entity.DTOs;
+
+namespace VehicleBreakdownListRecord.API.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+                    if (error != null)
+                    {
+                        var statusCode = error.Error switch
+                        {
+                            ClientSideException clientSideException => 400,
+                            NotFoundException notFoundException => 404,
+                            _ => 500
+                        };
+                        context.Response.StatusCode = statusCode;
+                        var response = CustomResultDto<NoContentDto>.Fail(statusCode, error.Error.Message);
+
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                    }
+                });
+            });
+        }
+    }
+}
