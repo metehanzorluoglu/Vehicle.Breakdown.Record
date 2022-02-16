c1bd490341a5f75f632e5e539a2b9167367189e0
Metehan Zorluoglu <metehanzorluoglu@gmail.com>
2022-02-16 12:34:55 +0300

    Adding LoginMigration

--- Vehicle.Breakdown.Record.DAL/Migrations/20220216090140_AddLoginTables.Designer.cs
+++ Vehicle.Breakdown.Record.DAL/Migrations/20220216090140_AddLoginTables.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VehicleBreakdownRecord.DAL;
 
 namespace VehicleBreakdownRecord.DAL.Migrations
 {
     [DbContext(typeof(VehicleDbContext))]
-    partial class VehicleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220216090140_AddLoginTables")]
+    partial class AddLoginTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
